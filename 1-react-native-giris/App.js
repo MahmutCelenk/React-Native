35f3f174cef47eded703e944d95d1f9cb35d571e
MahmutCelenk <mahmuttv63@gmail.com>
2026-04-08 15:19:38 +0300

    fix page

--- 1-react-native-giris/App.js
+++ 1-react-native-giris/App.js
@@ -5,6 +5,10 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 import HomeScreen from './src/screens/HomeScreen';
 import CoursesScreen from './src/screens/CoursesScreen';
+import CoursesInformationScreen from './src/screens/CoursesInformationScreen';
+import CounterScreen from './src/screens/CounterScreen';
+import BoxScreen from './src/screens/BoxScreen';  
+import ColorChangeScreen from './src/screens/ColorChangeScreen';
 
 const Stack = createNativeStackNavigator()
 
@@ -22,6 +26,10 @@ export default function App() {
       <Stack.Navigator initialRouteName='Ana Sayfa'>
         <Stack.Screen name="Ana Sayfa" component={HomeScreen} />
         <Stack.Screen name = 'Kurslarım' component={CoursesScreen} />
+        <Stack.Screen name = 'KursBilgilerim' component={CoursesInformationScreen} />
+        <Stack.Screen name = 'Sayac' component={CounterScreen} />
+        <Stack.Screen name = 'KutuUygulaması' component={BoxScreen} />
+        <Stack.Screen name = 'RenkDegistirme' component={ColorChangeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
